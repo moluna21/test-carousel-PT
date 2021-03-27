b17dadf0f130cb375a08971982c3d30d009d1e8f
C.Marina Luna <c.marinaluna@MacBook-Pro-de-CMarina.local>
2021-03-27 13:12:08 +0100

    Updated js & sass

--- js/carousel.js
+++ js/carousel.js
@@ -2,11 +2,10 @@ $(".owl-carousel").owlCarousel({
   loop:true,
   margin:24,
   nav:true,
-  responsiveClass:true,
-  responsive:{
-    0:{
-        items:1,
-        nav:true
-    },
-  }
+  animateOut: 'fadeOut',
+  autoplay:true,
+  autoplayTimeout:5000,
+  lazyLoad:true,
+  loop:true,
+  items:1,
 });
